d3afb77da1c8129e54cd8c567e57d10afd6556bf
hakatashi <hakatasiloving@gmail.com>
2014-05-08 12:01:34 +0900

    Add Number.prototype.pow and Number.prototype.log

--- src/number.js
+++ src/number.js
@@ -12,3 +12,15 @@ Number.prototype.toRadians = function () {
 Number.prototype.mod = function (n) {
     return ((this % n) + n) % n;
 };
+
+// Usage of Math.pow in prototype definition.
+Number.prototype.pow = function (n) {
+	return Math.pow(this, n);
+};
+
+// Take log_n of this number. If no arguments supplied, log_e(napier) is assumed.
+Number.prototype.pow = function (n) {
+	if (typeof n === 'undefined') n = Math.E;
+	if (n <= 0) return Number.NaN;
+	return Math.log(this) / Math.log();
+};
