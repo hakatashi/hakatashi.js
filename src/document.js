217297c132952d0fdb1ea75a16a83b40f9a22340
hakatashi <hakatasiloving@gmail.com>
2014-05-08 10:30:06 +0900

    Ready to use in node.js

--- src/document.js
+++ src/document.js
@@ -1,7 +1,10 @@
-// http://stackoverflow.com/questions/6466135/
-document.getParameterByName = function (name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-};
+// not activate in node
+if (typeof document !== 'undefined') {
+	// http://stackoverflow.com/questions/6466135/
+	document.getParameterByName = function (name) {
+		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+			results = regex.exec(location.search);
+		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	};
+}
